9e2ede7ae91388bc823ca9ebc0c18ad953c63883
Hello HongFu <hellohongfu@outlook.com>
2018-10-14 22:34:11 +0800

    解决一个安全的问题，是因为提交post请求时，crsf安全限制

--- src/main/java/hello/WebSecurityConfig.java
+++ src/main/java/hello/WebSecurityConfig.java
@@ -25,6 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        
         http
             .authorizeRequests()
                 .antMatchers("/css/*", "/home","/register","/restetPassword","/fileupload").permitAll()
@@ -36,6 +37,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .permitAll();
+        
+            
     }
 
     // @Bean
